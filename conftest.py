22d2ffb67ecf02d56e65a75955e93bd5642f813c
Qing1199 <woshiqing1@gmail.com>
2026-05-09 15:29:13 +0800

    Fix GitHub Actions Chrome headless mode

--- conftest.py
+++ conftest.py
@@ -1,11 +1,22 @@
 import pytest
 from selenium import webdriver
-from utils.config import BASE_URL
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get(BASE_URL)
+
+    chrome_options = Options()
+
+    # IMPORTANT FOR GITHUB ACTIONS
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get("https://www.saucedemo.com/")
+
     yield driver
+
     driver.quit()
